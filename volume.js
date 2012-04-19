56b84f3408b08b80a4e5e0055544f46570a0d6c4
David Thompson <dtt101@gmail.com>
2012-04-19 22:00:30 +0100

    getting rid of meteor for now

--- volume.js
+++ volume.js
@@ -1,6 +1,13 @@
 
 if (Meteor.is_client) {
-
+  Meteor.startup(function () {
+  	$('#container').isotope({
+  	  // options
+  	  itemSelector : '.item',
+  	  layoutMode : 'straightAcross'
+  	});
+  });
+	
   //Template.hello.greeting = function () {
   //  return "Welcome to volume.";
   //};
