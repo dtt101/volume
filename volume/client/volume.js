5e59f5d029f5c02dda34d6faa652a100084354f0
David Thompson <dtt101@gmail.com>
2012-04-22 21:13:55 +0100

    created example meteor app - towards #1

---
+++ volume/client/volume.js
@@ -0,0 +1,66 @@
+if (Meteor.is_client) {
+	
+	// fade item visibility
+	var fadeOutItems = function (selector) {
+	    $(selector).each(function() { 
+	        var item = $(this); 
+					if (item) {
+						item.fadeTo("slow", 0);
+					}
+	    });
+	}
+
+	var fadeInItems = function (selector) {	
+	    $(selector).each(function() { 
+	        var item = $(this); 
+					if (item) {
+						item.fadeTo("slow", 1);
+					}
+	    });
+	}
+
+	Meteor.startup(function () {
+		// run scrolling
+		$.stellar({
+		    // Set scrolling to be in either one or both directions
+		    horizontalScrolling: true,
+		    verticalScrolling: false	
+		});
+	
+		// play audio
+		// TODO - make ogg
+		var mySound = new buzz.sound("sounds/koko.mp3", {
+		    preload: true,
+		    autoplay: true,
+		    loop: false
+		});
+
+		// audio event handlers - 	// sound.getVolume()
+		$("#pause-audio").click(function() {
+			var text = 'pause';
+			mySound.togglePlay();
+			updateAudioUI();
+		});
+	
+		$("#up-audio").click(function() {
+			mySound.increaseVolume(10);
+		});
+
+		$("#down-audio").click(function() {
+			mySound.decreaseVolume(10);
+		});
+	
+		var updateAudioUI = function () {
+			if (mySound.isPaused()) {
+				text = 'play';
+			} else {
+				text = 'pause';			
+			}
+			$('#pause-audio').html(text);
+		}
+
+		// call initially to set correct state for play button
+		updateAudioUI();
+
+    });	
+}
